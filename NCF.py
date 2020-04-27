f740e0389d4c4141a3624f1e094124aa1b764695
Andrea Guidi <ing.guidiandrea@gmail.com>
2020-04-27 20:03:36 +0200

    removed maskedSumOfSquares

--- NCF.py
+++ NCF.py
@@ -5,8 +5,7 @@ import numpy as np
 
 class NCF(gluon.Block):
     
-    
-    def __init__(self,user_vocabulary,user_emb_size,item_vocabulary,item_emb_size,n_hidden_layers,hidden_units,n_outputs,**kwargs):
+    def __init__(self,user_vocabulary,user_emb_size,item_vocabulary,item_emb_size,hidden_units,n_outputs,**kwargs):
         
         super(NCF,self).__init__(**kwargs)
         
@@ -16,75 +15,55 @@ class NCF(gluon.Block):
                                                output_dim=user_emb_size)
             self.item_emb = gluon.nn.Embedding(input_dim=item_vocabulary+1,
                                                output_dim=item_emb_size)
-
-            n_hidden_layers = n_hidden_layers
             hidden_units = hidden_units
-            
-            if n_hidden_layers != len(hidden_units):
+            n_layers = len(hidden_units)
                 
-                raise ValueError("You have to specify as many hidden units as layers")
+            for i in range(n_layers):
                 
-            #for i in range(n_hidden_layers):
-                
-            #    self.__dict__[f'inner_dense_{i+1}'] = gluon.nn.Dense(units=hidden_units[i], activation='relu')
+                self.__dict__[f'dense_{i+1}'] = gluon.nn.Dense(units=hidden_units[i], activation='relu')
+                self.__dict__['_children'][f'dense_{i+1}'] = self.__dict__[f'dense_{i+1}']
         
-            self.dense_1 = gluon.nn.Dense(units=hidden_units[0], activation='relu')
-            self.dense_2 = gluon.nn.Dense(units=hidden_units[1], activation='relu')
             self.out_layer = gluon.nn.Dense(units=n_outputs)
                     
     def forward(self, x,y):
         
         x = self.user_emb(x)
-        print(f"shape of x:{x}")
         y = self.item_emb(y)
-        print(f"shape of y:{y}")
-        latent_vector = nd.concat(x,y, dim=1)
-        print(f"shape of concatenated:{latent_vector}")
-
+        latent_vector = nd.elemwise_mul(x,y)
         hid = self.dense_1(latent_vector)
-        
+        hid = self.dense_2(hid)
+        hid = self.dense_3(hid)
         out = self.out_layer(hid)
         
         return(out)
-                    
-        
-                    
-                    
-class MaskedSumOfSquares(gluon.loss.Loss):
-    
-    def __init__(self,**kwargs):
-        super(MaskedSumOfSquares, self).__init__(weight=None,batch_axis=0,**kwargs)
-        
-    def forward(self,output,label):
-        
-        mask = nd.greater(label,0)
-        
-        masked_output = nd.elemwise_mul(output,mask)
-        
-        return nd.sum(nd.abs(masked_output-label)**2)         
-            
+                                  
+########################################################################################
 
 class InputDataset(gluon.data.Dataset):
     
-    def __init__(self,data):
-        
-        if not isinstance(data, mx.ndarray.ndarray.NDArray):
-            
-            raise TypeError("data must be of type nd.array")
-        
+    def __init__(self,data, columns=None):
+    
         self.length = len(data)
+        # Supposing that if it's not numpy or ndarray then it's a dataframe
+        if not isinstance(data,mx.ndarray.ndarray.NDArray):
+            self.data = _dataframe_to_ndarray(data,columns)
         self.data = data
         
     def __getitem__(self, idx):
         
         userId = self.data[idx,0].astype('int32')
         itemId = self.data[idx,1].astype('int32')
-        rating = self.data[idx,2].astype('int8')
+        rating = self.data[idx,2]
         
         return userId,itemId,rating
     
     def __len__(self):
         
         return self.length
+    
+    
+    def _dataframe_to_ndarray(data, columns):
+        
+        return nd.array(data[columns])
     
         
